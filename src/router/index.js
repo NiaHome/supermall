2fd8c8fab27d9fc3e33c749ef5f499f369e72192
apple <zhenzhen>
2020-10-30 17:21:10 +0800

    修改样式

---
+++ src/router/index.js
@@ -0,0 +1,48 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+
+const Home = () => import('../views/home/Home')
+const Cart = () => import('../views/cart/Cart')
+const Category = () => import('../views/category/Category')
+const Profile = () => import('../views/profile/Profile')
+
+//1.安装插件
+Vue.use(Router)
+
+//2.创建路由对象
+const routes = [
+  {
+    path: '',
+    redirect:'/home'
+  },
+  {
+    path: '/home',
+    name: '首页',
+    component: Home
+  },
+  {
+    path: '/cart',
+    name: '购物车',
+    component: Cart
+  },
+  {
+    path: '/profile',
+    name: '档案',
+    component: Profile
+  },
+  {
+    path: '/category',
+    name: '分类',
+    component: Category
+  }
+]
+
+const router = new Router({
+  //配置路径和组建的应用关系
+  routes,
+  //去掉url中的#号
+  mode: 'history'
+})
+
+//3.导出
+export default router
--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -1,13 +1,95 @@
 <template>
-<h2>首页</h2>
+<div id="home">
+  <nav-bar class="home-nav"><div slot="center">购物街</div></nav-bar>
+  <home-swiper :banners="banners"/>
+  <home-recommend-view :recommends="recommends"/>
+  <feature-view/>
+  <tab-control class="tab-control" :titles="titles"/>
+
+  <ul>
+    <li>item1</li>
+    <li>item1</li>
+    <li>item1</li>
+    <li>item1</li>
+    <li>item1</li>
+    <li>item1</li>
+    <li>item1</li>
+    <li>item1</li>
+    <li>item1</li>
+    <li>item1</li>
+    <li>item1</li>
+    <li>item1</li>
+    <li>item1</li>
+    <li>item1</li>
+    <li>item1</li>
+    <li>item1</li>
+    <li>item1</li>
+    <li>item1</li>
+    <li>item1</li>
+    <li>item1</li>
+    <li>item1</li>
+    <li>item1</li>
+    <li>item1</li>
+    <li>item1</li>
+    <li>item1</li>
+    <li>item1</li>
+    <li>item1</li>
+  </ul>
+</div>
 </template>
 
 <script>
-    export default {
-        name: "Home"
+  import NavBar from 'components/common/navBar/NavBar'
+  import TabControl from 'components/content/tabControl/TabControl'
+
+  import HomeSwiper from './childrenComponents/HomeSwiper'
+  import HomeRecommendView from './childrenComponents/HomeRecommendView'
+  import FeatureView from './childrenComponents/FeatureView'
+
+  import {getHomeMultiData} from "network/home";
+
+
+  export default {
+    name: "Home",
+    components: {
+      NavBar,
+      HomeSwiper,
+      HomeRecommendView,
+      FeatureView,
+      TabControl
+    },
+    data() {
+      return {
+        banners: [],
+        recommends: [],
+        titles: ['流行', '新款', '精选']
+      }
+    },
+    created() {
+      //1.请求多个数据
+      getHomeMultiData().then(res => {
+        console.log(res.recommend);
+        this.banners = res.banner;
+        this.recommends = res.recommend
+      })
     }
+  }
 </script>
 
 <style scoped>
+ .home-nav {
+   background-color: var(--color-tint);
+   color: white;
 
+   position: fixed;
+   left: 0;
+   right:0;
+   top: 0;
+   z-index: 9;
+ }
+
+  .tab-control {
+    position: sticky;
+    top:44px;
+  }
 </style>
